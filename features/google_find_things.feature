de1b94b5382379561895ba9d8078e4937ea488c9
Maryna Kovalenko <maryna.kovalenko@globallogic.com>
2015-10-23 18:31:48 +0300

    Adds gem 'random-word' and new steps description

--- features/google_find_things.feature
+++ features/google_find_things.feature
@@ -1,4 +1,4 @@
-Feature: google finds things
+Feature: Google finds things
 
 In order to get some knowledge about a sought-for thing
 As a user of Google search engine
@@ -6,18 +6,16 @@ I want Google to find me information about that thing
 
 Scenario: Result from Wiki
 Given a word for search
-When I go to Google web site
-And ask Google to find something
+When I go to Google web site and ask Google to find something about my word
 Then I find a Wikipedia page among search results
 And go Wiki page
 Then I make a screenshot of this page if description of sought-for word is correct
 
 
-Scenario: Result not from Wiki
-Given a word for search
-When I go to Google web site
-And ask Google to find something
-Then I go to the first web site from given results, but not Wiki
-Then I make a screenshot of this page if description of sought-for word is correct
+# Scenario: Result not from Wiki
+# Given a word for search
+# When I go to Google web site and ask Google to find something about my word
+# Then I go to the first web site from given results, but not Wiki
+# Then I make a screenshot of this page if description of sought-for word is correct
 
 
--- features/step_definitions/google_steps.rb
+++ features/step_definitions/google_steps.rb
@@ -2,35 +2,54 @@ require 'capybara'
 require 'capybara/cucumber'
 require 'selenium-webdriver'
 require 'site_prism'
+require 'random-word'
+
+Capybara.default_driver = :selenium
 
 class Word
+  def generate_word
+    @word = RandomWord.nouns.next
+  end
 end
 
 class Google < SitePrism::Page
     set_url "http://www.google.com"
     element :search_field, "input[name='q']"
-    element :search_button, "button[name='btnK']"
+    element :search_button, "button[name='btnG']"
 end
 
-Given /^a word$/ do
-    @word = Word.new
+class Wiki < SitePrism::Page
+    element
 end
-When /^I go to Google web site$/ do
+
+class SearchResults < SitePrism::Page
+    set_url_matcher /google.com\/results\?.*/
+    # section :menu, MenuSection,"#gbx3"
+    # sections :search_results, SearchResultSection, "#results li"
+end
+
+
+Given /^a word for search$/ do
+    @example = Word.new.generate_word
+end
+When /^I go to Google web site and ask Google to find something about my word$/ do
     @page = Google.new
+    @page.load
+    @page.search_field.set @example
+    @page.search_button.click
+
 end
-And /^ask Google to find something$/ do
-end
-Then /^I find a Wikipedia page among search results$/ do
+Then /^I find a Wikipedia page among results$/ do
+    @res_page = SearchResults.new
+    expect(@res_page).to be_displayed
+    expect(@res_page.search_result_links).to include "http://en.wikipedia.org/wiki/#{@example}"
 end
 And /^go Wiki page$/ do
+    @wiki = Wiki.new
+
 end
 Then /^I make a screenshot of this page iff description of soughtfor word is correct$/ do
 end
 
 
 
-# class SearchResults < SitePrism::Page
-#     set_url_matcher /google.com\/results\?.*/
-#     section :menu, MenuSection, "#gbx3"
-#     sections :search_results, SearchResultSection, "#results li"
-# end
