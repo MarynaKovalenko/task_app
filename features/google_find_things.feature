651e7caf51cb8571a3dadf2e67fcffd08c792069
Maryna Kovalenko <maryna.kovalenko@globallogic.com>
2015-10-30 18:50:45 +0200

    Adds new changes

--- features/google_find_things.feature
+++ features/google_find_things.feature
@@ -4,18 +4,19 @@ In order to get some knowledge about a sought-for thing
 As a user of Google search engine
 I want Google to find me information about that thing
 
-Scenario: Result from Wiki
-Given a word for search
-When I go to Google web site and ask Google to find something about my word
-Then I find a Wikipedia page among search results
-And go Wiki page
-Then I make a screenshot of this page if description of sought-for word is correct
+# Scenario: Result from Wiki
+# Given a word for search and Google web-page
+# When I ask Google to find something about my word
+# Then I get some results
+# Then I go to Wiki page(s) containing information about that word and make a screenshot
 
 
-# Scenario: Result not from Wiki
-# Given a word for search
-# When I go to Google web site and ask Google to find something about my word
-# Then I go to the first web site from given results, but not Wiki
+
+Scenario: Result not from Wiki
+Given a word for search and Google web-page
+When I ask Google to find something about my word
+Then I get some results
+When I go to the first web site from given results, but non Wiki-page
 # Then I make a screenshot of this page if description of sought-for word is correct
 
 
--- features/step_definitions/google_steps.rb
+++ features/step_definitions/google_steps.rb
@@ -2,54 +2,70 @@ require 'capybara'
 require 'capybara/cucumber'
 require 'selenium-webdriver'
 require 'site_prism'
-require 'random-word'
+require 'faker'
+require 'byebug'
+require 'capybara-screenshot/cucumber'
 
 Capybara.default_driver = :selenium
 
 class Word
   def generate_word
-    @word = RandomWord.nouns.next
+    @word = Faker::Hacker.noun
   end
 end
 
-class Google < SitePrism::Page
-    set_url "http://www.google.com"
+class GoogleHome < SitePrism::Page
+    set_url "http://www.google.com/en"
     element :search_field, "input[name='q']"
     element :search_button, "button[name='btnG']"
 end
 
 class Wiki < SitePrism::Page
-    element
+    element :heading, "#firstHeading"
 end
 
-class SearchResults < SitePrism::Page
+class GoogleResults < SitePrism::Page
     set_url_matcher /google.com\/results\?.*/
-    # section :menu, MenuSection,"#gbx3"
-    # sections :search_results, SearchResultSection, "#results li"
+    elements :result_links, ".r>a"
+
+    def find_wiki_links
+        self.all(:css, ".r>a[href*='wikipedia']").map { |l| l[:href] }.uniq
+    end
 end
 
 
-Given /^a word for search$/ do
-    @example = Word.new.generate_word
-end
-When /^I go to Google web site and ask Google to find something about my word$/ do
-    @page = Google.new
+Given /^a word for search and Google web-page$/ do
+    p @example = Word.new.generate_word
+    @page = GoogleHome.new
+    @page.has_search_field?
+    @page.has_search_button?
     @page.load
+end
+When /^I ask Google to find something about my word$/ do
     @page.search_field.set @example
     @page.search_button.click
 
 end
-Then /^I find a Wikipedia page among results$/ do
-    @res_page = SearchResults.new
-    expect(@res_page).to be_displayed
-    expect(@res_page.search_result_links).to include "http://en.wikipedia.org/wiki/#{@example}"
-end
-And /^go Wiki page$/ do
-    @wiki = Wiki.new
-
-end
-Then /^I make a screenshot of this page iff description of soughtfor word is correct$/ do
+Then /^I get some results$/ do
+    @res_page = GoogleResults.new
+    @res_page.displayed?
+    @res_page.has_result_links?
 end
 
+Then /^I go to Wiki page(s) containing information about that word and make a screenshot$/ do
+    @wiki_links = res_page.find_wiki_links
+    @wiki_links.each_with_index do |link, index|
+        visit link
+        @wiki = Wiki.new
+        @wiki.heading.text.downcase.include? @example
+        @wiki.save_screenshot("wikipage_#{@wiki.heading.text}_#{Time.now}")
+    end
+end
+
+When /^I go to the first web site from given results, but non Wiki-page$/ do
+    byebug
+    visit @res_page.result_links.map{|l| l[:href]}.delete_if{|l| l.downcase.include?('wikipedia')}.first
+    save_screenshot("#{}")
+end
 
 
